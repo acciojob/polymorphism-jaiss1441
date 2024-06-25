aa96d8da3ab4ea025286a269ba7b66767c998871
jais1441 <jaiswalvikas7276@gmail.com>
2024-06-25 21:57:52 +0530

    Task Completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,29 @@
 package com.driver;
 
+class Product{
+    public int product(int x, int y) {
+
+        return 0;
+    }
+    public int product(int x, int y, int z) {
+
+        return 0;
+    }
+
+    public double product(double x, double y) {
+        return 0;
+    }
+}
+
 public class Main {
+
+    public static void main(String[] args){
+        Product p =new Product();
+        p.product(11,21);
+        p.product(12,32,12);
+        p.product(22,33);
+    }
+
+
 
 }
